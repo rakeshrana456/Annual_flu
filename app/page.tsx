9716152403b895ab8f7b3075ba0946374620c8d1
rakeshrana456 <127132518+rakeshrana456@users.noreply.github.com>
2025-07-07 13:04:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ export default function AnnualFluShots() {
           >
             <Button className="bg-green-600 hover:bg-green-700 text-white">
               <FontAwesomeIcon icon={faWhatsapp} className="w-5 h-5 mr-2" />
-              WhatsApp
+              Book now
             </Button>
           </a>
         </div>
@@ -150,7 +150,7 @@ export default function AnnualFluShots() {
             >
               <Button className="w-full bg-green-600 hover:bg-green-700 text-white">
                 <FontAwesomeIcon icon={faWhatsapp} className="w-5 h-5 mr-2" />
-                WhatsApp
+                Book now
               </Button>
             </a>
           </div>
@@ -221,7 +221,7 @@ export default function AnnualFluShots() {
                       icon={faWhatsapp}
                       className="w-5 h-5 mr-2"
                     />
-                    WhatsApp
+                    Book now
                   </Button>
                 </a>
               </div>
@@ -1290,7 +1290,7 @@ export default function AnnualFluShots() {
                         icon={faWhatsapp}
                         className="w-6 h-6 mr-3"
                       />
-                       WhatsApp
+                       Book now
                     </Button>
                   </a>
                 </div>
